4dafec5d1d8ccb4bf6193be3c7474d164ed23444
João Guilherme Lins <162372169+Guilherm03@users.noreply.github.com>
2024-09-02 16:47:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let primeiroNome = "John";
+let sobrenome = "Wick";
+let curso = "Desenvolvimento de Sistemas";
+    
+const anoNascimento = 1989;
+    
+let idade = 2024 - anoNascimento;
+    
+console.log(`Olá ${primeiroNome} ${sobrenome}
+Então você estuda ${curso} e tem ${idade} anos. Legal!
+        
+`);
+    
+
+let number1 = 5
+let number2 = 2
+    
+let soma = number1 + number2;
+let subtracao = number1 - number2;
+let mulplicacao = number1 * number2;
+let divisao = number1 / number2;
+    
+console.log(`Resultados:
+${number1} + ${number2} = ${soma}
+${number1} - ${number2} = ${subtracao}
+${number1} * ${number2} = ${mulplicacao}
+${number1} / ${number2} = ${divisao}
+`)
